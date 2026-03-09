742dcd0604294000e889d3faf03fcad3d43333d9
Kevalkumar <kgi@mm-software.com>
2026-03-09 10:26:10 +0530

    refactor the middleware testcase to remove error

--- source/AAS.TwinEngine.DataEngine.UnitTests/Infrastructure/Http/Authorization/Middleware/HeaderSanitizationMiddlewareTests.cs
+++ source/AAS.TwinEngine.DataEngine.UnitTests/Infrastructure/Http/Authorization/Middleware/HeaderSanitizationMiddlewareTests.cs
@@ -13,27 +13,23 @@ public class HeaderSanitizationMiddlewareTests
     [Fact]
     public async Task InvokeAsync_CallsValidateIncomingHeaders_AndNext()
     {
-        var services = new ServiceCollection();
         var mappingService = Substitute.For<IRequestHeaderMapper>();
-        _ = services.AddSingleton(mappingService);
-        var serviceProvider = services.BuildServiceProvider();
 
-        var context = new DefaultHttpContext
-        {
-            RequestServices = serviceProvider
-        };
+        var context = new DefaultHttpContext();
 
         var nextCalled = false;
-        Task Next(HttpContext _) { nextCalled = true; return Task.CompletedTask; }
+
+        Task Next(HttpContext _)
+        {
+            nextCalled = true;
+            return Task.CompletedTask;
+        }
 
         var middleware = new HeaderSanitizationMiddleware(Next);
 
-        await middleware.InvokeAsync(context).ConfigureAwait(false);
+        await middleware.InvokeAsync(context, mappingService);
 
-        mappingService
-            .Received(1)
-            .ValidateIncomingHeaders(context);
-
+        mappingService.Received(1).ValidateIncomingHeaders(context);
         Assert.True(nextCalled);
     }
 }
