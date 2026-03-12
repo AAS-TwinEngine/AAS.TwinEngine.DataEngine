45acc3891bb03de4d09467d2cc5118ee6951044e
Kevalkumar <kgi@mm-software.com>
2026-03-12 17:26:37 +0530

    added check for health endpoint to ignore in header validation middleware

--- source/AAS.TwinEngine.DataEngine/Infrastructure/Http/Authorization/Middleware/HeaderSanitizationMiddleware.cs
+++ source/AAS.TwinEngine.DataEngine/Infrastructure/Http/Authorization/Middleware/HeaderSanitizationMiddleware.cs
@@ -4,6 +4,7 @@ namespace AAS.TwinEngine.DataEngine.Infrastructure.Http.Authorization.Middleware
 
 public class HeaderSanitizationMiddleware(RequestDelegate next)
 {
+    private const string HealthCheckPath = "/healthz";
     private readonly RequestDelegate _next = next;
 
     public async Task InvokeAsync(HttpContext context, IRequestHeaderMapper requestHeaderMapper)
@@ -11,8 +12,16 @@ public class HeaderSanitizationMiddleware(RequestDelegate next)
         ArgumentNullException.ThrowIfNull(context);
         ArgumentNullException.ThrowIfNull(requestHeaderMapper);
 
+        if (IsHealthCheckRequest(context))
+        {
+            await _next(context).ConfigureAwait(false);
+            return;
+        }
+
         requestHeaderMapper.ValidateIncomingHeaders(context);
 
         await _next(context).ConfigureAwait(false);
     }
+
+    private static bool IsHealthCheckRequest(HttpContext context) => context.Request.Path.StartsWithSegments(HealthCheckPath, StringComparison.OrdinalIgnoreCase);
 }
