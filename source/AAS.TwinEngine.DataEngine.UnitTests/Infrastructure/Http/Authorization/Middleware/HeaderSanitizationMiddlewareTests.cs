d3568dd1d8ebc3bbd5d7bd173b260750c622042f
Kevalkumar <kgi@mm-software.com>
2026-03-12 17:34:34 +0530

    Added Test Cases for Header Middleware to ignore health endpoint

--- source/AAS.TwinEngine.DataEngine.UnitTests/Infrastructure/Http/Authorization/Middleware/HeaderSanitizationMiddlewareTests.cs
+++ source/AAS.TwinEngine.DataEngine.UnitTests/Infrastructure/Http/Authorization/Middleware/HeaderSanitizationMiddlewareTests.cs
@@ -10,10 +10,9 @@ namespace AAS.TwinEngine.DataEngine.UnitTests.Infrastructure.Http.Authorization;
 public class HeaderSanitizationMiddlewareTests
 {
     [Fact]
-    public async Task InvokeAsync_CallsValidateIncomingHeaders_AndNext()
+    public async Task InvokeAsync_WhenNormalRequest_ShouldValidateHeaders_AndCallNext()
     {
-        var mappingService = Substitute.For<IRequestHeaderMapper>();
-
+        var mapper = Substitute.For<IRequestHeaderMapper>();
         var context = new DefaultHttpContext();
 
         var nextCalled = false;
@@ -26,9 +25,33 @@ public class HeaderSanitizationMiddlewareTests
 
         var middleware = new HeaderSanitizationMiddleware(Next);
 
-        await middleware.InvokeAsync(context, mappingService);
+        await middleware.InvokeAsync(context, mapper);
 
-        mappingService.Received(1).ValidateIncomingHeaders(context);
+        mapper.Received(1).ValidateIncomingHeaders(context);
+        Assert.True(nextCalled);
+    }
+
+    [Fact]
+    public async Task InvokeAsync_WhenHealthCheckRequest_ShouldSkipValidation_AndCallNext()
+    {
+        var mapper = Substitute.For<IRequestHeaderMapper>();
+
+        var context = new DefaultHttpContext();
+        context.Request.Path = "/healthz";
+
+        var nextCalled = false;
+
+        Task Next(HttpContext _)
+        {
+            nextCalled = true;
+            return Task.CompletedTask;
+        }
+
+        var middleware = new HeaderSanitizationMiddleware(Next);
+
+        await middleware.InvokeAsync(context, mapper);
+
+        mapper.DidNotReceive().ValidateIncomingHeaders(Arg.Any<HttpContext>());
         Assert.True(nextCalled);
     }
 }
