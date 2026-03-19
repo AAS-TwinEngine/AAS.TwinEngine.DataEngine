db4843b07012c88aa45316309457c99b9b1692bb
Kevalkumar <kgi@mm-software.com>
2026-03-19 14:51:23 +0530

    Remove MultiLanguagePropertySettings options binding and validation from dependency injection

--- source/AAS.TwinEngine.DataEngine/ServiceConfiguration/InfrastructureDependencyInjectionExtensions.cs
+++ source/AAS.TwinEngine.DataEngine/ServiceConfiguration/InfrastructureDependencyInjectionExtensions.cs
@@ -60,11 +60,6 @@ public static class InfrastructureDependencyInjectionExtensions
         _ = services.AddHttpClientWithoutResilience(AasEnvironmentConfig.AasRegistryHealthCheckHttpClientName, aasEnvironment?.AasRegistryBaseUrl!);
         _ = services.AddHttpClientWithoutResilience(AasEnvironmentConfig.SubmodelRegistryHealthCheckHttpClientName, aasEnvironment?.SubModelRegistryBaseUrl!);
 
-        _ = services.AddOptions<MultiLanguagePropertySettings>()
-            .Bind(configuration.GetSection(MultiLanguagePropertySettings.Section))
-            .ValidateOnStart();
-        _ = services.AddSingleton<IValidateOptions<MultiLanguagePropertySettings>, MultiLanguagePropertySettingsValidator>();
-
         foreach (var plugin in plugins.Plugins)
         {
             _ = services.AddHttpClientWithResilience(configuration, PluginConfig.HttpClientNamePrefix + plugin.PluginName, HttpRetryPolicyOptions.PluginDataProvider, plugin.PluginUrl);
