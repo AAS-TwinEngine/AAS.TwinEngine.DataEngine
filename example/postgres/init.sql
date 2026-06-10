5ab54c7643243ade51f646dba82faf4bd6b6a27b
Hardi Shah <hsh@mm-software.com>
2026-06-10 14:22:27 +0530

    Refactor contact information schema and related data
    - Deleted the "ContactInformation" schema and all related tables (Phone, Fax, Email, IPCommunication, AssetContactInformation).
    - Updated the initialization script to remove references to the deleted contact information schema and replaced it with "MaintenanceInstructions".
    - Modified the development configuration to reflect the change from "ContactInformation" to "MaintenanceInstructions" in the template ID.

--- example/postgres/init.sql
+++ example/postgres/init.sql
@@ -7,7 +7,7 @@
 -- Execution Order:
 --   1. 01_core_asset_tables.sql.inc    - Create core asset database schema
 --   2. 02_nameplate_carbonfootprint_technicaldata.sql.inc -  Create Nameplate, Carbon Footprint, Technical Data tables and insert data
---   3. 03_contactinformations.sql.inc - Create Contact information and relationships table and insert data
+--   3. 03_MaintenanceInstructions.sql.inc - Create Maintenance Instructions and relationships table and insert data
 --   4. 04_handoverdocumentation.sql.inc - Create Handover Documentation table and insert data
 --
 -- ============================================================
@@ -18,8 +18,8 @@
 \echo 'Executing: 02_nameplate_carbonfootprint_technicaldata.sql.inc - Inserting  data...'
 \i /docker-entrypoint-initdb.d/02_nameplate_carbonfootprint_technicaldata.sql.inc
 
-\echo 'Executing: 03_contactinformations.sql.inc - Inserting contact information...'
-\i /docker-entrypoint-initdb.d/03_contactinformations.sql.inc
+\echo 'Executing: 03_MaintenanceInstructions.sql.inc - Inserting contact information...'
+\i /docker-entrypoint-initdb.d/03_MaintenanceInstructions.sql.inc
 
 \echo 'Executing: 04_handoverdocumentation.sql.inc - Inserting document metadata...'
 \i /docker-entrypoint-initdb.d/04_handoverdocumentation.sql.inc
