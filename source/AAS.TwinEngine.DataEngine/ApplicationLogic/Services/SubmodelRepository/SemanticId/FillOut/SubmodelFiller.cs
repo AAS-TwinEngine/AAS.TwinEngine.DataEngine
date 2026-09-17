ac780a4a891501f4dc72bdef53ff67bb24323588
Ankit Shah <ash@mm-software.com>
2026-09-17 18:46:22 +0530

    Optimize submodel bulk retrieval

--- source/AAS.TwinEngine.DataEngine/ApplicationLogic/Services/SubmodelRepository/SemanticId/FillOut/SubmodelFiller.cs
+++ source/AAS.TwinEngine.DataEngine/ApplicationLogic/Services/SubmodelRepository/SemanticId/FillOut/SubmodelFiller.cs
@@ -1,4 +1,6 @@
-﻿using AAS.TwinEngine.DataEngine.ApplicationLogic.Exceptions.Application;
+﻿using System.Collections.Concurrent;
+
+using AAS.TwinEngine.DataEngine.ApplicationLogic.Exceptions.Application;
 using AAS.TwinEngine.DataEngine.ApplicationLogic.Services.SubmodelRepository.SemanticId.ElementHandlers;
 using AAS.TwinEngine.DataEngine.ApplicationLogic.Services.SubmodelRepository.SemanticId.Helpers;
 using AAS.TwinEngine.DataEngine.ApplicationLogic.Services.SubmodelRepository.SemanticId.Helpers.Interfaces;
@@ -14,6 +16,8 @@ public class SubmodelFiller(
     IEnumerable<ISubmodelElementTypeHandler> handlers,
     ILogger<SubmodelFiller> logger) : ISubmodelFiller
 {
+    private readonly ConcurrentDictionary<Type, ISubmodelElementTypeHandler> _handlerByElementType = new();
+
     public ISubmodel FillOutTemplate(ISubmodel submodelTemplate, SemanticTreeNode values)
     {
         if (submodelTemplate is null)
@@ -137,7 +141,10 @@ if (values is null)
 
     private ISubmodelElement FillOutElement(ISubmodelElement element, SemanticTreeNode values, IReadOnlyDictionary<SemanticTreeNode, SemanticValueIndex> semanticValueIndexes)
     {
-        var handler = handlers.FirstOrDefault(h => h.CanHandle(element));
+        var handler = _handlerByElementType.GetOrAdd(
+            element.GetType(),
+            _ => handlers.FirstOrDefault(h => h.CanHandle(element))
+                ?? throw new InternalDataProcessingException());
         if (handler == null)
         {
             logger.LogError("InValid submodelElementTemplate Type. IdShort : {IdShort}", element.IdShort);
