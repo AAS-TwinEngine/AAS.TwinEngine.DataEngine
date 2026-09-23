3b7f3b77c680e6ed138f51772df93d38d284a44f
Kevalkumar <kgi@mm-software.com>
2026-09-23 17:23:15 +0530

    Refactor error messages for clarity and update logging level to Debug

--- source/AAS.TwinEngine.DataEngine/ApplicationLogic/Exceptions/Application/PluginCapabilityNotSupportedException.cs
+++ source/AAS.TwinEngine.DataEngine/ApplicationLogic/Exceptions/Application/PluginCapabilityNotSupportedException.cs
@@ -4,7 +4,7 @@ namespace AAS.TwinEngine.DataEngine.ApplicationLogic.Exceptions.Application;
 
 public class PluginCapabilityNotSupportedException : NotImplementedException
 {
-    public const string ServiceName = "Plugin Capability Not Supported.";
+    public const string ServiceName = "Functionality Not Supported.";
 
     public PluginCapabilityNotSupportedException() : base(ServiceName) { }
 
